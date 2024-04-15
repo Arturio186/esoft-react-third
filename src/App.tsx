df9089d728a3df2f32731e1b1898de3fe8a855b0
Arturio186 <bain.romanoff@mail.ru>
2024-04-15 19:03:46 +0500

    WindowSize

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.scss'
 
 import CountdownTimer from '#components/CountdownTimer/CountdownTimer'
 import UserList from '#components/UserList/UserList'
+import WindowSize from '#components/WindowSize/WindowSize'
 
 const App : React.FC = () => {
     return (
         <>
             <CountdownTimer timerStart={10} />
             <UserList />
+            <WindowSize />
         </>
     )
 }
